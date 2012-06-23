890837a675b29b6d98ac346323943516439d8ae2
John Cupitt <jcupitt@gmail.com>
2012-06-23 10:37:31 +0100

    branch 0.1 off, add dev vips
    now looks for dev vips (7.29) ready for new mem min stuff to go in

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -4,7 +4,7 @@ require "mkmf"
 
 File::unlink("Makefile") if (File::exist?("Makefile"))
 
-%w{7.28 7.27 7.26 7.24 7.23 7.22 7.20}.each{|x| break if pkg_config("vips-#{x}")}
+%w{7.29 7.28 7.27 7.26 7.24 7.23 7.22 7.20}.each{|x| break if pkg_config("vips-#{x}")}
 
 have_header('vips/vips.h')
 create_makefile('vips_ext')
