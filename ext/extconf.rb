e7355ff0b92eebd1830a960d807d6c037c62af35
stanislaw <s.pankevich@gmail.com>
2012-06-25 19:57:31 +0300

    raise if there is no pkg_config for proper libvips versions available

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -4,7 +4,9 @@ require "mkmf"
 
 File::unlink("Makefile") if (File::exist?("Makefile"))
 
-%w{7.29 7.28 7.27 7.26 7.24 7.23 7.22 7.20}.each{|x| break if pkg_config("vips-#{x}")}
+VIPS_VERSIONS = %w[7.29 7.28 7.27 7.26 7.24 7.23 7.22 7.20]
+
+raise("There is no pkg_config for any of following libvips versions: #{VIPS_VERSIONS.join(', ')}") unless VIPS_VERSIONS.detect {|x| pkg_config("vips-#{x}") }
 
 have_header('vips/vips.h')
 create_makefile('vips_ext')
