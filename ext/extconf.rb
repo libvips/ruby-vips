d38c46361524508acead58981e12b5a0bc91a21b
John Cupitt <jcupitt@gmail.com>
2012-06-25 18:09:16 +0100

    better vips pkg-config test
    thanks stanislaw for the much nicer code

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -4,16 +4,8 @@ require "mkmf"
 
 File::unlink("Makefile") if (File::exist?("Makefile"))
 
-found = false
-%w{7.29 7.28 7.27 7.26 7.24 7.23 7.22 7.20}.each do |x| 
-    if pkg_config("vips-#{x}")
-        found = true
-        break
-    end
-end
+VIPS_VERSIONS = %w[7.29 7.28 7.27 7.26 7.24 7.23 7.22 7.20]
 
-if not found
-    puts "no compatible libvips found, check your pkg-config settings"
-else
-    create_makefile('vips_ext')
-end
+raise("There is no pkg_config for any of following libvips versions: #{VIPS_VERSIONS.join(', ')}") unless VIPS_VERSIONS.detect {|x| pkg_config("vips-#{x}") }
+
+create_makefile('vips_ext')
