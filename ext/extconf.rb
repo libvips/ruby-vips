8e9e775ac0c6c5e72d3126f4938e0cdf5ccb9816
John Cupitt <jcupitt@gmail.com>
2012-06-25 17:55:46 +0100

    fail sooner if no vips found
    Thanks Pierre

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -4,7 +4,16 @@ require "mkmf"
 
 File::unlink("Makefile") if (File::exist?("Makefile"))
 
-%w{7.29 7.28 7.27 7.26 7.24 7.23 7.22 7.20}.each{|x| break if pkg_config("vips-#{x}")}
+found = false
+%w{7.29 7.28 7.27 7.26 7.24 7.23 7.22 7.20}.each do |x| 
+    if pkg_config("vips-#{x}")
+        found = true
+        break
+    end
+end
 
-have_header('vips/vips.h')
-create_makefile('vips_ext')
+if not found
+    puts "no compatible libvips found, check your pkg-config settings"
+else
+    create_makefile('vips_ext')
+end
