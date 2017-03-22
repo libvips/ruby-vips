8b4a8029d77532e48917d58846ebde9eb00fa63f
John Cupitt <jcupitt@gmail.com>
2017-03-22 11:02:31 +0000

    no need to list the tar contents

--- install-vips.sh
+++ install-vips.sh
@@ -3,7 +3,7 @@
 set -e
 if [ ! -d "$HOME/vips/lib" ]; then
 	wget $VIPS_SITE/$VIPS_VERSION/vips-$VIPS_VERSION_FULL.tar.gz
-	tar xvf vips-$VIPS_VERSION_FULL.tar.gz
+	tar xf vips-$VIPS_VERSION_FULL.tar.gz
 	cd vips-$VIPS_VERSION_FULL
 	CXXFLAGS=-D_GLIBCXX_USE_CXX11_ABI=0 ./configure --prefix=$HOME/vips 
 	make && make install
