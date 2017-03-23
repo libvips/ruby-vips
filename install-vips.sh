f2ad9cc3a94974d0d952bde9cebc489306f450db
John Cupitt <jcupitt@gmail.com>
2017-03-23 13:35:36 +0000

    try to simplify .travis.yml

--- install-vips.sh
+++ install-vips.sh
@@ -1,23 +1,27 @@
 #!/bin/bash 
 
+vips_site=http://www.vips.ecs.soton.ac.uk/supported
+version=$VIPS_VERSION_MAJOR.$VIPS_VERSION_MINOR
+version_full=$VIPS_VERSION.$VIPS_VERSION_MICRO
+
 set -e
 
 # do we already have the correct vips built? early exit if yes
 # we could check the configure params as well I guess
 if [ -d "$HOME/vips/bin" ]; then
-	version=$($HOME/vips/bin/vips --version)
+	installed_version=$($HOME/vips/bin/vips --version)
 	escaped_version="$VIPS_VERSION_MAJOR\.$VIPS_VERSION_MINOR\.$VIPS_VERSION_MICRO"
-	echo "Need vips-$VIPS_VERSION_FULL"
-	echo "Found $version"
-	if [[ "$version" =~ ^vips-$escaped_version ]]; then
+	echo "Need vips-$version_fulL"
+	echo "Found $installed_version"
+	if [[ "$installed_version" =~ ^vips-$escaped_version ]]; then
 		echo "Using cached directory"
 		exit 0
 	fi
 fi
 
 rm -rf $HOME/vips
-wget $VIPS_SITE/$VIPS_VERSION/vips-$VIPS_VERSION_FULL.tar.gz
-tar xf vips-$VIPS_VERSION_FULL.tar.gz
-cd vips-$VIPS_VERSION_FULL
+wget $vips_site/$version/vips-$version_full.tar.gz
+tar xf vips-$version_full.tar.gz
+cd vips-$version_full
 CXXFLAGS=-D_GLIBCXX_USE_CXX11_ABI=0 ./configure --prefix=$HOME/vips $*
 make && make install
