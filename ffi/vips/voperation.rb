a7bde486ae1be8e29bcfe587543252a1b8a1279e
John Cupitt <jcupitt@gmail.com>
2017-07-14 13:33:38 +0100

    call sort-of done, needs testing

--- ffi/vips/voperation.rb
+++ ffi/vips/voperation.rb
@@ -81,45 +81,129 @@ module Vips
             return nil
         end
 
-        def self.call(name, option_string, supplied_values)
+        def self.call(name, option_string, supplied)
             op = new_from_name name
             throw get_error if op == nil
 
+            # find and classify all the arguments the operator can take
             args = get_construct_args
-
-            # count required input args
-            n_required_input = 0 
+            required_input = [] 
+            optional_input = {}
+            required_output = [] 
+            optional_output = {}
             args.each do |name, flags|
-                if (flags & ARGUMENT_REQUIRED) != 0 and
-                    (flags & ARGUMENT_INPUT) != 0 and
-                    (flags & ARGUMENT_DEPRECATED) == 0 
-                    n_required_input += 1 
+                next if (flags & ARGUMENT_DEPRECATED) != 0
+
+                if (flags & ARGUMENT_INPUT) != 0 
+                    if (flags & ARGUMENT_REQUIRED) != 0 and
+                        required_input << [name, flags]
+                    else
+                        optional_input[name] = flags
+                    end
+                end
+
+                if (flags & ARGUMENT_OUTPUT) != 0 
+                    if (flags & ARGUMENT_REQUIRED) != 0 and
+                        required_output << [name, flags]
+                    else
+                        optional_output[name] = flags
+                    end
                 end
             end
 
             # so we should have been supplied with n_required_input values, or
             # n_required_input + 1 if there's a hash of options at the end
-            if supplied_values.length == n_required_input 
-                optional_values = nil
-            elsif supplied_values.length == n_required_input + 1 and
-                supplied_values.last.is_a? Hash
-                optional_values = supplied_values.last
-                supplied_values.delete_at -1
+            if supplied.length == required_input.length 
+                supplied_optional = nil
+            elsif supplied.length == required_input.length + 1 and
+                supplied.last.is_a? Hash
+                supplied_optional = supplied.last
+                supplied.delete_at -1
             else
                 throw "unable to call #{name}: " + 
-                    "you supplied #{supplied_values.length} arguments, " +
-                    "but operation needs #{n_required_input}."
+                    "you supplied #{supplied.length} arguments, " +
+                    "but operation needs #{required_input.length}."
             end
+
+            # FIXME .. need to verify that all supplied_optional keys are in
+            # either optional_input or optional_output
 
             # the first image arg is the thing we expand constants to match ...
             # we need to find it
             #
             # look inside array and hash arguments, since we may be passing an
             # array of images
-            match_image = find_inside(supplied_values) do |value|
+            match_image = find_inside(supplied) do |value|
                 value.is_a VipsImage
             end
 
+            # set any string args first so they can't be overridden
+            if option_string != nil
+                if Vips::vips_object_set_from_string(op, option_string) != 0
+                    throw "unable to call #{name}: #{Vips::get_error}"
+                end
+            end
+
+            # set all required inputs
+            (0...required_input.length) do |i|
+                arg_name = required_input[i][0]
+                flags = required_input[i][1]
+                value = supplied[i]
+
+                # FIXME ... need to use match_image
+                # FIXME ... need to use flags
+                op.set arg_name, value
+            end
+
+            # set all optional inputs
+            if supplied_optional
+                supplied_optional.each do |arg_name, value|
+                    if optional_input.has_key? arg_name
+                        flags = optional_input[arg_name]
+
+                        # FIXME ... need to use match_image
+                        # FIXME ... need to use flags for MODIFY etc.
+                        op.set arg_name, value
+                    end
+                end
+            end
+
+            # build and adjust counts
+            op2 = Vips::vips_cache_operation_build op 
+            if op2 == nil
+                throw "unable to call #{name}: #{Vips::get_error}"
+            end
+            op2 = VipsOperation.new op2
+            op = op2
+            op2 = nil
+
+            # get all required results, then all optional ones
+            # FIXME ... need to fetch modified inputs too
+            result = []
+            required_output.each do |arg_name, flags|
+                result << op.get arg_name
+            end
+
+            optional_results = {}
+            if supplied_optional
+                supplied_optional.each do |arg_name, value|
+                    if optional_output.has_key? arg_name
+                        flags = optional_output[arg_name]
+
+                        optional_results[arg_name] = op.get arg_name
+                    end
+                end
+            end
+
+            result << optional_results if optional_results != {}
+
+            if result.length == 1
+                result = result.first
+            elsif result.length == 0
+                result = nil
+            end
+
+            return result
         end
 
     end
@@ -134,5 +218,7 @@ module Vips
                                          :pointer, :pointer], :pointer
 
     attach_function :vips_operation_new, [:string], :pointer
+
+    attach_function :vips_cache_operation_build, [:pointer], :pointer
 
 end
