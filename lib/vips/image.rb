1f1ee38838c13c8bf93cc912a49a0656e6a3031f
Janko Marohnić <janko.marohnic@gmail.com>
2018-07-09 22:13:57 +0200

    Fix Vips::Image#composite RDocs

--- lib/vips/image.rb
+++ lib/vips/image.rb
@@ -1046,8 +1046,9 @@ module Vips
 
         # Composite a set of images with a set of blend modes.
         #
-        # @param other [Image, Array<Image>, Real, Array<Real>] bands to append
-        # @return [Image] many band image
+        # @param other [Image, Array<Image>] images to composite
+        # @param mode [Symbol, Array<Symbol>] blend modes to use
+        # @return [Image] blended image
         def composite other, mode, opts = {}
             unless other.is_a? Array
                 other = [other]
