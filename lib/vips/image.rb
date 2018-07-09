ca9435471728e055b585aff4724c5a8123c88957
Janko Marohnić <janko.marohnic@gmail.com>
2018-07-09 22:45:46 +0200

    Specify available options for Image#composite

--- lib/vips/image.rb
+++ lib/vips/image.rb
@@ -1048,6 +1048,9 @@ module Vips
         #
         # @param overlay [Image, Array<Image>] images to composite
         # @param mode [BlendMode, Array<BlendMode>] blend modes to use
+        # @param opts [Hash] Set of options
+        # @option opts [Vips::Interpretation] :compositing_space Composite images in this colour space
+        # @option opts [Boolean] :premultiplied Images have premultiplied alpha
         # @return [Image] blended image
         def composite overlay, mode, opts = {}
             unless overlay.is_a? Array
