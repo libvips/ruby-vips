2647fff9f470b4edb45f9d88057e1b34649696bd
John Cupitt <jcupitt@gmail.com>
2019-02-22 11:06:10 +0000

    make watermark use composite

--- example/watermark.rb
+++ example/watermark.rb
@@ -4,7 +4,7 @@ require 'vips'
 
 im = Vips::Image.new_from_file ARGV[0], access: :sequential
 
-# make the text overlay mask
+# make the text mask
 text = Vips::Image.text ARGV[2], width: 500, dpi: 300, font: 'sans bold'
 text = text.rotate(-45)
 # make the text transparent
@@ -13,21 +13,11 @@ text = text.gravity :centre, 200, 200
 text = text.replicate 1 + im.width / text.width, 1 + im.height / text.height
 text = text.crop 0, 0, im.width, im.height
 
-# we blend in RGB colourspace, so no CMYK etc.
-im = im.colourspace :srgb
+# we make a constant colour image and attach the text mask as the alpha
+overlay = (text.new_from_image [255, 128, 128]).copy interpretation: :srgb
+overlay = overlay.bandjoin text
 
-# we want to split the image into alpha and non-alpha so we can blend the
-# text into the main part of the image and leave any alpha untouched
-if im.has_alpha?
-  alpha = im[im.bands - 1]
-  im = im[0...im.bands - 1]
-else
-  alpha = nil
-end
-
-im = text.ifthenelse [255, 128, 128], im, blend: true
-
-# reattach alpha
-im = im.bandjoin alpha if alpha
+# overlay the text
+im = im.composite overlay, :over
 
 im.write_to_file ARGV[1]
