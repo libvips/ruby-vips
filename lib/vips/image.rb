8355160ceac8c50c6bc5f7af08d420365d8305a2
John Cupitt <jcupitt@gmail.com>
2019-06-12 18:11:12 +0100

    reenable rubocop on commit

--- lib/vips/image.rb
+++ lib/vips/image.rb
@@ -385,8 +385,8 @@ module Vips
     def new_from_image value
       pixel = (Vips::Image.black(1, 1) + value).cast(format)
       image = pixel.embed 0, 0, width, height, extend: :copy
-      image.copy interpretation: interpretation,
-          xres: xres, yres: yres, xoffset: xoffset, yoffset: yoffset
+      image.copy interpretation: interpretation, xres: xres, yres: yres,
+        xoffset: xoffset, yoffset: yoffset
     end
 
     # Write this image to a file. Save options may be encoded in the
