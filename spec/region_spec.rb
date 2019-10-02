634a560aad85d08e8ee9e6cc504ef198a4e73188
John Cupitt <jcupitt@gmail.com>
2019-10-02 16:49:15 +0100

    test libvips version for region spec

--- spec/region_spec.rb
+++ spec/region_spec.rb
@@ -8,21 +8,25 @@ RSpec.describe Vips::Region do
     expect(region)
   end
 
-  it 'can fetch pixels from a region' do
-    image = Vips::Image.black(100, 100)
-    region = Vips::Region.new image
-    pixel_data = region.fetch 10, 20, 30, 40
+  if Vips::at_least_libvips?(8, 8)
+    it 'can fetch pixels from a region' do
+      image = Vips::Image.black(100, 100)
+      region = Vips::Region.new image
+      pixel_data = region.fetch 10, 20, 30, 40
 
-    expect(pixel_data)
-    expect(pixel_data.length == 30 * 40)
+      expect(pixel_data)
+      expect(pixel_data.length == 30 * 40)
+    end
   end
 
-  it 'can make regions with width and height' do
-    image = Vips::Image.black(100, 100)
-    region = Vips::Region.new image
-    pixel_data = region.fetch 10, 20, 30, 40
+  if Vips::at_least_libvips?(8, 8)
+    it 'can make regions with width and height' do
+      image = Vips::Image.black(100, 100)
+      region = Vips::Region.new image
+      pixel_data = region.fetch 10, 20, 30, 40
 
-    expect(region.width == 30)
-    expect(region.height == 40)
+      expect(region.width == 30)
+      expect(region.height == 40)
+    end
   end
 end
