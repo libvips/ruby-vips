665f78bd2456613593b4e1bfaf3fe5ad3f7502c1
John Cupitt <jcupitt@gmail.com>
2019-11-13 08:48:15 +0000

    fix typo in comments

--- lib/vips.rb
+++ lib/vips.rb
@@ -614,7 +614,7 @@ module Vips
   LIBRARY_VERSION = Vips::version_string
 
   # libvips has this arbitrary number as a sanity-check upper bound on image
-  # size. It's sometimes useful for know whan calculating image ratios.
+  # size. It's sometimes useful to know when calculating scale factors.
   MAX_COORD = 10000000
 end
 
