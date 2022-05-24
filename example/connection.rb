04709222758e199c8c77654326dc2c97a91e82f9
John Cupitt <jcupitt@gmail.com>
2022-05-24 16:41:30 +0100

    add extended targetcustom support
    but TIFF write fails for some reason argh

--- example/connection.rb
+++ example/connection.rb
@@ -1,26 +1,42 @@
 #!/usr/bin/ruby
 
 require "vips"
-require "down/http"
+# gem install down
+require "down"
 
 # byte_source = File.open ARGV[0], "rb"
 # eg. https://images.unsplash.com/photo-1491933382434-500287f9b54b
-byte_source = Down::Http.open(ARGV[0])
+byte_source = Down::open(ARGV[0])
 
 source = Vips::SourceCustom.new
 source.on_read do |length|
-  puts "reading #{length} bytes ..."
+  puts "source: reading #{length} bytes ..."
   byte_source.read length
 end
 source.on_seek do |offset, whence|
-  puts "seeking to #{offset}, #{whence}"
+  puts "source: seeking to #{offset}, #{whence}"
   byte_source.seek(offset, whence)
 end
 
-byte_target = File.open ARGV[1], "wb"
+byte_target = File.open ARGV[1], "w+b"
+
 target = Vips::TargetCustom.new
-target.on_write { |chunk| byte_target.write(chunk) }
-target.on_finish { byte_target.close }
+target.on_write do |chunk| 
+  puts "target: writing #{chunk.length} bytes ..."
+  byte_target.write(chunk) 
+end
+target.on_read do |length| 
+  puts "target: reading #{length} bytes ..."
+  byte_target.read length 
+end
+target.on_seek do |offset, whence| 
+  puts "target: seeking to #{offset}, #{whence}"
+  byte_target.seek(offset, whence) 
+end
+target.on_end do 
+  puts "target: ending"
+  byte_target.close 
+end
 
 image = Vips::Image.new_from_source source, "", access: :sequential
-image.write_to_target target, ".jpg"
+image.write_to_target target, ARGV[2]
--- lib/vips/targetcustom.rb
+++ lib/vips/targetcustom.rb
@@ -66,8 +66,63 @@ module Vips
       end
     end
 
+    # The block is executed to read data from the target. The interface is
+    # exactly as IO::read, ie. it takes a maximum number of bytes to read and
+    # returns a string of bytes from the target, or nil if the target is already
+    # at end of file.
+    #
+    # This handler is optional and only needed for image formats which have
+    # to be able to read their own output, like TIFF.
+    #
+    # @yieldparam length [Integer] Read and return up to this many bytes
+    # @yieldreturn [String] Up to length bytes of data, or nil for EOF
+    def on_read &block
+      # target read added in 8.13
+      if Vips.at_least_libvips?(8, 13)
+        signal_connect "read" do |buf, len|
+          chunk = block.call len
+          return 0 if chunk.nil?
+          bytes_read = chunk.bytesize
+          buf.put_bytes(0, chunk, 0, bytes_read)
+          chunk.clear
+
+          bytes_read
+        end
+      end
+    end
+
+    # The block is executed to seek the target. The interface is exactly as
+    # IO::seek, ie. it should take an offset and whence, and return the
+    # new read position.
+    #
+    # This handler is optional and only needed for image formats which have
+    # to be able to read their own output, like TIFF.
+    #
+    # @yieldparam offset [Integer] Seek offset
+    # @yieldparam whence [Integer] Seek whence
+    # @yieldreturn [Integer] the new read position, or -1 on error
+    def on_seek &block
+      # target seek added in 8.13
+      if Vips.at_least_libvips?(8, 13)
+        signal_connect "seek" do |offset, whence|
+          block.call offset, whence
+        end
+      end
+    end
+
     # The block is executed at the end of write. It should do any necessary
-    # finishing action, such as closing a file.
+    # finishing action, such as closing a file, and return 0 on sucess and -1
+    # on error
+    #
+    # @yieldreturn [Integer] 0 on sucess, or -1 on error
+    def on_end &block
+      signal_name = Vips.at_least_libvips?(8, 13) ? "end" : "finish"
+      signal_connect signal_name do
+        block.call
+      end
+    end
+
+    # Deprecated name for libvips before 8.13
     def on_finish &block
       signal_connect "finish" do
         block.call
